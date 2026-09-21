5fd6d3d2ca46324350701b82909a900d938f02fd
manishshriwas-01 <manish.shriwas@ongraph.com>
2026-09-21 15:40:49 +0530

    Navbar fixed

--- client/src/app/components/navbar/navbar.ts
+++ client/src/app/components/navbar/navbar.ts
@@ -1,10 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
